54096b1ac05a80300bf56acc18d83d5f448850b8
GarvielL0ken <garviel.loken.42@gmail.com>
2022-07-20 10:59:48 +0200

    Products imported from data.js. The page will not print and error message if a specified product is not found

--- site/js/view-product.js
+++ site/js/view-product.js
@@ -1,3 +1,4 @@
+import { products } from "./data/data.js"
 import { Product } from "./classes/Product.js";
 import { getURLParameter } from "./lib.js"
 
@@ -5,27 +6,18 @@ var application = new Vue({
 	el : "#application",
 	data : {
 		title : '',
-		products : [
-			new Product(true, "Lorem", 'Garvielloken.jpg', 'Garviel Loken', 'Always so straight up and down'),
-			new Product(true, "ipsum", 'TarikTorgaddon.jpg', 'Tarik Torgaddon', 'Do you really think you have a chance? Really? Your yellow ragamuffins against the best of the best?'),
-			new Product(true, "dolor"),
-			new Product(true, "sit"),
-			new Product(true, "amet"),
-			new Product(true, "consectetur"),
-			new Product(true, "adipiscing"),
-			new Product(true, "elit")
-		]
+		products : products
 	},
 	methods : {
 		printProduct() {
 			if (this.selectedProduct)
 				return (this.selectedProduct.printFullView());
 			else
-				return ('');
+				return ('<h1>Error: Product Not Found</h1>');
 		}
 	},
 	mounted() {
 		this.title = getURLParameter(window.location.href, "title");
-		this.selectedProduct = Product.getProductByName(this.products, this.title); 
+		this.selectedProduct = Product.getProductByName(this.products, this.title);
 	}
 })
