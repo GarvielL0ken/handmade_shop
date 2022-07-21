6e99b01b9099227c42781b806e69797a6b851c42
GarvielL0ken <garviel.loken.42@gmail.com>
2022-07-21 11:16:39 +0200

    Added static localStorageKey. Constructor no longer takes parameter 'itemsInCart'. Constructor attempts to load from localStorage otherwise it returns a default instance. Added addProduct() which appends a new type Product to this.products. Added save() which saves the instance to localStorage

--- site/js/classes/Cart.js
+++ site/js/classes/Cart.js
@@ -1,7 +1,38 @@
+import { Product } from "./Product.js";
+
 export class Cart {
-	constructor(verbose, itemsInCart) {
+	static localStorageKey = "cart";
+
+	constructor(verbose) {
+		var strCart;
+		var objCart;
+
+		this.size = 0;
+		this.products = [];
+
 		this.verbose_ = verbose;
 
-		this.itemsInCart = itemsInCart;
+		strCart = localStorage.getItem(Cart.localStorageKey);
+		if (!strCart)
+			return;
+
+		objCart = JSON.parse(strCart);
+		this.size = objCart.size;
+		this.loadProducts(objCart.products);
+	}
+
+	loadProducts(objProducts) {
+		objProducts.forEach(product => {
+			this.addProduct(Product.parseProduct(product));
+		});
+	}
+
+	addProduct(product) {
+		this.products.push(product);
+		this.save();
+	}
+
+	save() {
+		localStorage.setItem(Cart.localStorageKey, JSON.stringify(this));
 	}
 }
