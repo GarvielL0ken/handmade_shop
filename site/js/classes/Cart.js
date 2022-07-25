78b8f234e2b3b8541fc3cfa775b026e2e198d6a7
GarvielL0ken <garviel.loken.42@gmail.com>
2022-07-25 14:48:32 +0200

    Employs Currency class, cleaed up comments in constructor function. Made comments a little clearer. Implements save() method. Implements deleteProductById method

--- site/js/classes/Cart.js
+++ site/js/classes/Cart.js
@@ -1,23 +1,27 @@
 import { Product } from "./Product.js";
+import { Currency } from "../lib.js";
 
 export class Cart {
-	static localStorageKey = "cart";
-	static verbose = false;
+	static	/*string*/	localStorageKey = "cart";
+	static	/*bool*/	verbose = false;
 
-	/*-----------------------------------------------------------------------*/
-	/*Construct a default instance and attempt to load data from localStorage*/
-	/*
-	 *@param	{}	;
-	 */
-	/*-----------------------------------------------------------------------*/
+	/*------------------------------CONSTRUCTOR-------------------------------/
+	 *Construct a default instance and attempt to load data from localStorage /
+	 *@param	{}	;                                                         /
+	 *@return	{Cart}                                                        /
+	/*------------------------------CONSTRUCTOR------------------------------*/
 	constructor() {
-		var strCart;
-		var objCart;
+		/*PRIVATE*/
+		/*bool*/	this.verbose_ = Cart.verbose;
 
-		this.size = 0;
-		this.products = [];
-
-		this.verbose_ = Cart.verbose;
+		/*PUBLIC*/
+		/*array*/	this.products = [];
+		/*float*/	this.total = new Currency(0.0);
+		/*int*/		this.size = 0;
+		
+		/*String*/	var	strCart;
+		
+		/*Object*/	var	objCart;
 
 		strCart = localStorage.getItem(Cart.localStorageKey);
 		if (!strCart)
@@ -28,28 +32,18 @@ export class Cart {
 		this.loadProducts(objCart.products);
 	}
 
-	/*-----------------------------------------------------------------------*/
-	/*Takes an object generated from JSON.parse and adds each element as a   */
-	/*new type Product to this.products                                      */
-	/*
-	 *@param	{object}	objProducts;
-	 */
+
+	/*------------------------------------------------------------------------/
+	 *Takes an object generated from JSON.parse and adds each element as a    /
+	 *new type Product to this.products                                       /
+	 *@param	{object}	objProducts;                                      /
+	 *@return	{void}		                                                  /
 	/*-----------------------------------------------------------------------*/
 	loadProducts(objProducts) {
+		this.size = 0;
 		objProducts.forEach(product => {
 			this.addProduct(Product.parseProduct(product));
 		});
-	}
-
-	/*-----------------------------------------------------------------------*/
-	/*Takes a type Product and adds it to this.products                      */
-	/*
-	 *@param	{Product}	product;
-	 */
-	/*-----------------------------------------------------------------------*/
-	addProduct(product) {
-		this.products.push(product);
-		this.save();
 	}
 
 	/*-----------------------------------------------------------------------*/
@@ -59,6 +53,38 @@ export class Cart {
 	 */
 	/*-----------------------------------------------------------------------*/
 	save() {
+		console.log(JSON.stringify(this));
 		localStorage.setItem(Cart.localStorageKey, JSON.stringify(this));
+	}
+
+	/*-----------------------------------------------------------------------*/
+	/*Takes a type Product and adds it to this.products                      */
+	/*
+	 *@param	{Product}	product;
+	 */
+	/*-----------------------------------------------------------------------*/
+	addProduct(product) {
+		this.size += 1;
+		this.total.add(product.price);
+		this.products.push(product);
+		this.save();
+	}
+
+	/*-----------------------------------------------------------------------*/
+	/*Removes a product specified by its id                                  */
+	/*
+	 *@param	{int}	productId;
+	 */
+	/*-----------------------------------------------------------------------*/
+	deleteProductById(productId) {
+		this.products = this.products.filter((product) => {
+			if (product.idEquals(productId)) {
+				this.size -= 1;
+				this.total.subtract(product.price);
+				return (false);
+			}
+			return (product);
+		});
+		this.save();
 	}
 }
