5d6c59ba4f5c80aaef65a8de9fec9d748876c980
GarvielL0ken <garviel.loken.42@gmail.com>
2022-07-25 14:38:43 +0200

    Added the removeFromCart method which takes a product and calls this.cart.deleteProductById, passing the products id

--- site/js/cart.js
+++ site/js/cart.js
@@ -1,6 +1,7 @@
 import { Cart } from "./classes/Cart.js";
 import { Product } from "./classes/Product.js";
 import { products } from "./data/data.js";
+import { Currency } from "./lib.js";
 
 var application = new Vue({
 	el : "#application",
@@ -8,6 +9,9 @@ var application = new Vue({
 		cart : new Cart(true)
 	},
 	methods : {
+		removeFromCart(product) {
+			this.cart.deleteProductById(product.id_);
+		}
 	},
 	mounted() {
 	}
