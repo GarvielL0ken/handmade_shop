2f919d5b76cac332b021c5ea0dda41a90c24024f
GarvielL0ken <garviel.loken.42@gmail.com>
2022-07-26 15:06:54 +0200

    Removed unnecessary imports

--- site/js/cart.js
+++ site/js/cart.js
@@ -1,7 +1,4 @@
 import { Cart } from "./classes/Cart.js";
-import { Product } from "./classes/Product.js";
-import { products } from "./data/data.js";
-import { Currency } from "./lib.js";
 
 var application = new Vue({
 	el : "#application",
