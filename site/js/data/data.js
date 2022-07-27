1bd4801a2ca75c4bc4e8bac9bb01ce510ab76a92
GarvielL0ken <garviel.loken.42@gmail.com>
2022-07-27 16:41:27 +0200

    Added a few more products and new images

--- site/js/data/data.js
+++ site/js/data/data.js
@@ -1,14 +1,20 @@
 import { Product } from "../classes/Product.js"
 
 var products = [
-		new Product(true, "Lorem", 'New', 'Garvielloken.jpg', 'Garviel Loken', 'Always so straight up and down'),
-		new Product(true, "ipsum", 'Featured', 'TarikTorgaddon.jpg', 'Tarik Torgaddon', 'Do you really think you have a chance? Really? Your yellow ragamuffins against the best of the best?'),
-		new Product(true, "dolor", 'New;Featured'),
-		new Product(true, "sit"),
-		new Product(true, "amet"),
-		new Product(true, "consectetur"),
-		new Product(true, "adipiscing"),
-		new Product(true, "elit")
+		new Product("Luna Wolves on Davin", "Featured", "Dies Irae.jpg"),
+		new Product("Flight of the Eisenstein", "", "Flight of the Eisenstein.jpg"),
+		new Product("Horus Slays Sanguinius", "", "Emperor vs Horus.jpg"),
+		new Product("Garviel Loken", "Featured", 'Garvielloken.jpg', 'Garviel Loken', 'Always so straight up and down', 190.14),
+		new Product("Parade at Ullanor", "New", "Great Crusade.jpg"),
+		new Product("Imperator Class Titan", "New", "Imperator.jpg", "", "", 0),
+		new Product("Luna Wolf", "New", "Luna Wolf.webp"),
+		new Product("Luna Wolves Attack", "", "Luna Wolves Attack.png"),
+		new Product("Parade", '', 'parade.jfif', 'dolor', 'dolar?', 12.00),
+		new Product("Murder", "New;Featured", "Planet Murder.jpg"),
+		new Product("Tarrik Torgaddon", "Featured", 'TarrikTorgaddon.jpg', 'Tarrik Torgaddon', 'Do you really think you have a chance? Really? Your yellow ragamuffins against the best of the best?', 190.15),
+		new Product("Terminator Armor", "", "Terminator Armor.jpg"),
+		new Product("Titan Assault", "", "Titan Assault.jfif"),
+		new Product("Titan Legion", "", "Titan Legion.jpg")
 	]
 
 export { products }
